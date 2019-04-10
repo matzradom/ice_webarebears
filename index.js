84847b30f4ea4753cd771942a1b38cbe60bbacd1
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-10 21:12:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new Commando.Client({unknownCommandResponse: false});
 
 var xenon = ["Weirdest hooman that had ever lived.", "Xenon? Xenon? Idk him.", "AKA Cheeyin.", "He's actually a nice person. Sometimes."];
 var icetag = ["How bored can you be that you tag me for no reason?", "I don't know what you want. Srsly.", "I know I'm a bot but hoomans are dumb, tag me when you actually want me to do something."];
-var xenon = ["No.", "Play alone.", "I'm sick, I can't carry heavy loads."];
+var pubg = ["No.", "Play alone.", "I'm sick, I can't carry heavy loads."];
 
 bot.on('message' , function(message){
     if(message.content == bot.user + 'Hello')
