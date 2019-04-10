b4516ee8b7ed52266482b229e938c4468efe51ab
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-10 22:17:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const client = new Client();
 const bot = new Commando.Client({unknownCommandResponse: false});
 
 bot.on('message' , function(message){
-    if(message.content == 'Help Cmds')
+    if(message.content == 'help cmds')
     {
-        message.channel.sendMessage("Hi hooman, here are my commands, use them wisely ```Here are the commands that will need you to tag me first followed by a space.\n\n hello \n   - You will be receiving the best greeting in the world.\n\n Chill \n   - WIll ask Matz to chill.\n\n chill \n   - Will ask Xenon to chill.\n\n Bully \n   - Will ask Xenon to stop bullying Matz.\n\n bully \n   - WIll axk Matz to stop bullying Xenon.```");
+        message.channel.sendMessage("Hi hooman, here are my commands, use them wisely.\n```Here are the commands that will need you to tag me first followed by a space.\n\n hello \n   - You will be receiving the best greeting in the world.\n\n Chill \n   - WIll ask Matz to chill.\n\n chill \n   - Will ask Xenon to chill.\n\n Bully \n   - Will ask Xenon to stop bullying Matz.\n\n bully \n   - WIll axk Matz to stop bullying Xenon.```");
     }
 });
 
