f162c87b67e4643beaf94aa75cbad5690d4f32e2
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-11 08:17:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ bot.on('message' , function(message){
 var xenon = [" Weirdest hooman that had ever lived.", "Xenon? Xenon? Idk him.", "AKA Cheeyin.", "He's actually a nice person. Sometimes."];
 var icetag = [" How bored can you be that you tag me for no reason?", " I don't know what you want. Srsly.", " I know I'm a bot but hoomans are dumb, tag me when you actually want me to do something."];
 var pubg = [" No.", " Play alone.", " I'm sick, I can't carry heavy loads."];
+msg = message.content.toLowerCase();
 
 bot.on('message' , function(message){
-    if(message.content == bot.user + 'Hello')
+    if(msg == bot.user + ' hello')
     {
         message.channel.sendMessage("Hello Hooman, what's up?");
     }
@@ -54,14 +55,14 @@ bot.on('message' , function(message){
 
 
 bot.on('message' , function(message){
-    if(message.content == bot.user + ' Matz')
+    if(msg == bot.user + ' matz')
     {
         message.channel.sendMessage("Matz? My mom, my creator.");
     }
 });
 
 bot.on('message' , function(message){
-    if(message.content == bot.user + ' WRU')
+    if(msg == bot.user + ' wru')
     {
         message.channel.sendMessage("Hey " + message.author + ", I see you're interested in me hooman. My name is Ice, created on April 9, 2019 by my mom, creator, and an overall amazing hooman Matz. She is the only hooman I will ever love.\n\nFun fact, I was named after Ice Bear of We Bare Bears.");
     }
@@ -70,7 +71,7 @@ bot.on('message' , function(message){
 });
 
 bot.on('message' , function(message){
-    if(message.content == bot.user + ' Wake up Matz')
+    if(msg == bot.user + ' wake up matz')
     {
         message.channel.sendMessage('Mom, ' + message.author + ' wants you to wake up. I know, ' + message.author + ' sucks.');
     }
@@ -78,17 +79,9 @@ bot.on('message' , function(message){
   
 });
 
-bot.on('message' , function(message){
-    if(message.content == bot.user + ' wake up matz')
-    {
-        message.channel.sendMessage('Mom, ' + message.author + ' wants you to wake up. I know, ' + message.author + ' sucks.');
-    }
-
-  
-});
 
 bot.on('message' , function(message){
-    if(message.content == "pubg?")
+    if(msg == "pubg?")
     {
         var response = pubg[Math.floor(Math.random()*pubg.length)]
         message.channel.sendMessage(message.author + response).then().catch(console.error);
@@ -96,7 +89,7 @@ bot.on('message' , function(message){
 });
 
 bot.on('message' , function(message){
-    if(message.content == bot.user)
+    if(msg == bot.user)
     {
         var response = icetag[Math.floor(Math.random()*icetag.length)]
         message.channel.sendMessage(message.author + response).then().catch(console.error);
@@ -104,7 +97,7 @@ bot.on('message' , function(message){
 });
 
 bot.on('message' , function(message){
-    if(message.content == bot.user + ' Xenon')
+    if(msg == bot.user + ' xenon')
     {
         var response = xenon[Math.floor(Math.random()*xenon.length)]
         message.channel.sendMessage(response).then().catch(console.error);
@@ -114,7 +107,7 @@ bot.on('message' , function(message){
 
   bot.on('message', message => {
     // If the message is "how to embed"
-    if (message.content == bot.user + ' SOTD') {
+    if (msg == bot.user + ' sotd') {
       // We can create embeds using the MessageEmbed constructor
       // Read more about all that you can do with the constructor
       // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
@@ -135,7 +128,7 @@ bot.on('message' , function(message){
 
   bot.on('message', message => {
     // If the message is "how to embed"
-    if (message.content == bot.user + ' SOTD info') {
+    if (msg == bot.user + ' sotd info') {
       // We can create embeds using the MessageEmbed constructor
       // Read more about all that you can do with the constructor
       // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
