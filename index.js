c14c066610102bd07c0b7602b29cc5f33e98e132
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-11 12:06:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -119,12 +119,12 @@ bot.on('message' , function(message){
        
         .setTitle('i c e 。 Commands')
         .setColor(0xFF0000)
-        .setDescription("Hi " + message.author + " these are my commands, please use them wisely, I'm fragile, and fat.\n\n")                           .addField("https://i.ibb.co/b66hkMt/head.png")
+        .setDescription("Hi " + message.author + " these are my commands, please use them wisely, I'm fragile, and fat.\n\n")
                                 .addField("hello",
                                  "- Get a greeting back from me.\n\n")
                       
                                 .addField("bully",
-                                 "- Ok ok, I'll ask the person to stop bullying you.\n"+   "```@mention bully so I know who is bullying you.```")
+                                 "- Ok ok, I'll ask the person to stop bullying you.\n"+   "```@mention bully so I know who is bullying you.```", true)
       
                                 .addField("chill",
                                  "- Someone needs to chill?\n"+   "```@mention chill so I know who is <:supermeatblob:565003802772766740>.```")
