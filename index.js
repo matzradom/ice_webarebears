6ea3de159dc1ad44b8e0b0cddd875981db753bf5
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-13 18:33:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on('message' , async message => {
        
         .setTitle('Not too fast hooman.')
         .setColor(0xFF0000)
-        .setDescription((new Date()).getTime() - timeout.getTime())
+        .setDescription("Cooldown for this command is set to 1 minute.")
       
          message.channel.send(embed);
        } else {
