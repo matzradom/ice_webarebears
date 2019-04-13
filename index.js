78b2f5da06995c11ffa9a2cd8004de0b1b1d0570
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-13 22:53:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -281,7 +281,7 @@ bot.on('message' , function(message){
         {
         return;
         }
-        message.delete(0);
+        message.content.delete(0);
         message.mentions.users.first().sendMessage(message.content.slice (33));
         }
 });
