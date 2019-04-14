9e0162325bb61f2c08799592de5fd5288003a074
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-14 18:31:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -269,28 +269,7 @@ bot.on('message' , function(message){
         }     
 });
 
-bot.on('message' , function(message){
-        if(message.channel.type ==="dm") return;
-        if(message.content.toLowerCase() == pref + 'send meme'){
-exports.run = async (client, message, args) => {
-	try {
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-            .query({ limit: 800 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    } catch (err) {
-        return console.log(err);
-    }
+
 
 
 //-------------------------TEXT COMMANDS END---------------------------//
