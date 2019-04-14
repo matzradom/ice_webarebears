0e7708e900cfa52bc7a1ff74a4534b83b58f692a
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-14 19:57:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -270,23 +270,7 @@ bot.on('message' , function(message){
 }});
 
 
-bot.on('message' , async message => {
-	if(message.channel.type ==="dm") return;
-        if(message.content.toLowerCase() == pref + 'send meme'){
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
-            .query({ limit: 800 });
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    }
-});
+
 //-------------------------TEXT COMMANDS END---------------------------//
 
 
