f8e7e9b1f139b248f017e46e40c53b2b7efa6858
matzradom <49481963+matzradom@users.noreply.github.com>
2019-04-14 19:52:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -276,8 +276,6 @@ bot.on('message' , async message => {
         const { body } = await snekfetch
             .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
             .query({ limit: 800 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
         const randomnumber = Math.floor(Math.random() * allowed.length)
         const embed = new Discord.RichEmbed()
         .setColor(0x00A2E8)
