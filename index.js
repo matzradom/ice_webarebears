41c399e50f611455c43f6b6ad0bcd016fdbf2ddf
matzradom <49481963+matzradom@users.noreply.github.com>
2019-06-20 15:23:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Commando = require('discord.js-commando');
 const {Client, RichEmbed} = require('discord.js');
 const client = new Client();
 const bot = new Commando.Client({unknownCommandResponse: false});
-const snekfetch = require('snekfetch');
 const cooldownhello = new Set();
 const cooldownbully = new Set();
 const cooldownchill = new Set();
