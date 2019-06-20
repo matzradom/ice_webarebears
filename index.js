679b909f170b3b86692d04da6db88e85c3c800dd
matzradom <49481963+matzradom@users.noreply.github.com>
2019-06-20 22:18:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -622,12 +622,12 @@ bot.on('message', message => {
 
 bot.on('message', message => {
         if(message.channel.type ==="dm") return;
-        let modRole = message.guild.roles.find("name", "mod");
+        var modRole = message.guild.roles.find(role => role.name === "mod");
         if(message.member.roles.has(modRole.id)) {
         if(message.content.toLowerCase() == pref+'mute '+ message.mentions.users.first() ){
-        let muteRole = message.guild.roles.find("name", "normie");
+        var muteRole = message.guild.roles.find(role => role.name === "normie");
         let membermute = message.mentions.members.first();
-        message.membermute.removeRole(muteRole.id);
+        message.membermute.removeRole(muteRole);
         }
         } 
 });
