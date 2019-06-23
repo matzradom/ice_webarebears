131c68413dbfda8bce87f52511cf027dc9ebe492
matzradom <49481963+matzradom@users.noreply.github.com>
2019-06-23 11:55:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -626,7 +626,7 @@ bot.on('message', message => {
         if(message.content.toLowerCase() == pref+'mute '+ message.mentions.users.first() ){
         var modRole = message.guild.roles.find(role => role.name === "mod");
         const mod = message.author;
-        if(message.mod.roles.has(modRole.id)) {
+        if(message.member.roles.has(modRole.id)) {
         
         var muteRole = message.guild.roles.find(role => role.name === "normie");
         const mutemember = message.mentions.users.first();
