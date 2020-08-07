1d0bdcf8419c6b9ed2a4ba5702baad8b35e4c61d
matzradom <49481963+matzradom@users.noreply.github.com>
2020-08-07 15:47:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cooldowninfo = new Set();
 const cooldownwup = new Set();
 const cooldownbears = new Set();
 const cooldownpubg = new Set();
-var xenon = ["hmm test"];
+var xenon = ["hmm test", "お"];
 var icetag = ["<a:partyroblob:561806621995433985>", " How bored can you be that you tag me for no reason?", " I don't know what you want. Srsly.", " I know I'm a bot but hoomans are dumb, tag me when you actually want me to do something."];
 var bears = ["https://i.pinimg.com/564x/6c/66/01/6c6601ee0816cf07271d29c5b5d88b26.jpg",
              "https://i.pinimg.com/564x/d6/53/ce/d653ce37b5d00a5bb2ce6cf60da01799.jpg", 
